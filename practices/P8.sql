83ca7ca2aea2785ccdfae1ff15f4a37566358dee
Tung Bui <tungbq.info@gmail.com>
2021-10-23 17:09:45 +0700

    Add P8 solution

---
+++ practices/P8.sql
@@ -0,0 +1,4 @@
+-- Insert a record for your favorite Band and one of their Albums
+INSERT INTO bands(name) VALUE ('Westlife');
+INSERT into albums(name, release_year, band_id)
+VALUES ('Test albums', 2021, 8);
