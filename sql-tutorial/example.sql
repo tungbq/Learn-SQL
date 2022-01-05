1d376cbc8aeda50f73ffc7b8f7a03b1a5e53ce53
Tung Bui <tungbq.info@gmail.com>
2022-01-05 21:28:28 +0700

    Add basic queries

--- sql-tutorial/example.sql
+++ sql-tutorial/example.sql
@@ -4,20 +4,28 @@ CREATE TABLE student (
   name VARCHAR(40),
   major VARCHAR(40)
 );
+-- 
 DESCRIBE student;
+-- 
 ALTER TABLE student
 ADD gpa DECIMAL;
+-- 
 ALTER TABLE student DROP COLUMN gpa;
+-- 
 DROP TABLE student;
 -- insert data
 INSERT INTO student
 VALUES(1, 'Jack', 'Biology');
+-- 
 INSERT INTO student
 VALUES(2, 'Kate', 'Sociology');
+-- 
 INSERT INTO student(student_id, name)
 VALUES(3, 'Claire');
+-- 
 INSERT INTO student
 VALUES(4, 'Jack', 'Biology');
+-- 
 INSERT INTO student
 VALUES(5, 'Mike', 'Computer Science');
 -- show all rows in table
@@ -31,21 +39,27 @@ CREATE TABLE student (
   major VARCHAR(40) DEFAULT 'undecided',
   PRIMARY KEY (student_id)
 );
+-- 
 INSERT INTO student(name, major)
 VALUES("aaaa", 'Biology');
+-- 
 INSERT INTO student(student_id, name)
 VALUES(1, "Leo");
+-- 
 INSERT INTO student(name)
 VALUES(1, "Neymar");
 -- Update and delete
 SELECT *
 FROM student;
+-- 
 UPDATE student
 SET major = 'Bio'
 WHERE major = 'Biology';
+-- 
 UPDATE student
 SET major = 'undecided-1'
 WHERE student_id = 2;
+-- 
 UPDATE student
 SET name = 'Cong Phuong',
   major = 'Football'
@@ -53,5 +67,28 @@ WHERE student_id = 3;
 -- delete
 DELETE FROM student
 WHERE student_id = 3;
-
 DELETE FROM student;
+-- Basic queries
+SELECT *
+FROM student;
+-- 
+SELECT student.name,
+  student.major
+FROM student
+ORDER BY major DESC;
+-- 
+SELECT student.name,
+  student.major
+FROM student
+ORDER BY name ASC
+LIMIT 2;
+--
+SELECT *
+FROM student
+WHERE student_id >= 4;
+--
+SELECT *
+FROM student
+WHERE name IN ('Claire', 'Kate', 'Mike')
+  AND student_id > 2;
+--
