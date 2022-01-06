a89cdb99f4fa71699e35644710e67e52521f71fb
Tung Bui <tungbq.info@gmail.com>
2022-01-06 08:59:23 +0700

    Add data to table + use functions to query

--- sql-tutorial/sql-queries/company-db.sql
+++ sql-tutorial/sql-queries/company-db.sql
@@ -43,7 +43,7 @@ CREATE TABLE works_with (
   total_sales INT,
   PRIMARY KEY (emp_id, client_id),
   FOREIGN KEY (emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
-  FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE,
+  FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 -- create branch supplier table
 CREATE TABLE branch_supplier (
@@ -51,5 +51,233 @@ CREATE TABLE branch_supplier (
   supplier_name VARCHAR(40),
   supply_type VARCHAR(40),
   PRIMARY KEY (branch_id, supplier_name),
-  FOREIGN KEY (branch_id) REFERENCES banch(branch_id) ON DELETE CASCADE
-)
+  FOREIGN KEY (branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
+);
+-- feed data for all tables
+---------------------------------------------
+-- corporate
+INSERT INTO employee
+VALUES(
+    100,
+    'David',
+    'Wallace',
+    '1967-11-17',
+    'M',
+    250000,
+    NULL,
+    NULL
+  );
+-- 
+INSERT INTO branch
+VALUES(1, 'Corporate', 100, '2006-02-09');
+-- 
+UPDATE employee
+SET branch_id = 1
+WHERE emp_id = 100;
+-- 
+INSERT INTO employee
+VALUES(
+    101,
+    'Jan',
+    'Levinson',
+    '1961-05-11',
+    'F',
+    110000,
+    100,
+    1
+  );
+-- 
+------ Scranton
+INSERT INTO employee
+VALUES(
+    102,
+    'Michael',
+    'Scott',
+    '1964-03-15',
+    'M',
+    75000,
+    100,
+    NULL
+  );
+-- 
+INSERT INTO branch
+VALUES(2, 'Scranton', 102, '1992-04-06');
+UPDATE employee
+SET branch_id = 2
+WHERE emp_id = 102;
+-- 
+INSERT INTO employee
+VALUES(
+    103,
+    'Angela',
+    'Martin',
+    '1971-06-25',
+    'F',
+    63000,
+    102,
+    2
+  );
+-- 
+INSERT INTO employee
+VALUES(
+    104,
+    'Kelly',
+    'Kapoor',
+    '1980-02-05',
+    'F',
+    55000,
+    102,
+    2
+  );
+-- 
+INSERT INTO employee
+VALUES(
+    105,
+    'Stanley',
+    'Hudson',
+    '1958-02-19',
+    'M',
+    69000,
+    102,
+    2
+  );
+-- Stamford
+INSERT INTO employee
+VALUES(
+    106,
+    'Josh',
+    'Porter',
+    '1969-09-05',
+    'M',
+    78000,
+    100,
+    NULL
+  );
+INSERT INTO branch
+VALUES(3, 'Stamford', 106, '1998-02-13');
+UPDATE employee
+SET branch_id = 3
+WHERE emp_id = 106;
+INSERT INTO employee
+VALUES(
+    107,
+    'Andy',
+    'Bernard',
+    '1973-07-22',
+    'M',
+    65000,
+    106,
+    3
+  );
+INSERT INTO employee
+VALUES(
+    108,
+    'Jim',
+    'Halpert',
+    '1978-10-01',
+    'M',
+    71000,
+    106,
+    3
+  );
+----------------- BRANCH SUPPLIER
+INSERT INTO branch_supplier
+VALUES(2, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier
+VALUES(2, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier
+VALUES(3, 'Patriot Paper', 'Paper');
+INSERT INTO branch_supplier
+VALUES(2, 'J.T. Forms & Labels', 'Custom Forms');
+INSERT INTO branch_supplier
+VALUES(3, 'Uni-ball', 'Writing Utensils');
+INSERT INTO branch_supplier
+VALUES(3, 'Hammer Mill', 'Paper');
+INSERT INTO branch_supplier
+VALUES(3, 'Stamford Lables', 'Custom Forms');
+-- CLIENT
+INSERT INTO client
+VALUES(400, 'Dunmore Highschool', 2);
+INSERT INTO client
+VALUES(401, 'Lackawana Country', 2);
+INSERT INTO client
+VALUES(402, 'FedEx', 3);
+INSERT INTO client
+VALUES(403, 'John Daly Law, LLC', 3);
+INSERT INTO client
+VALUES(404, 'Scranton Whitepages', 2);
+INSERT INTO client
+VALUES(405, 'Times Newspaper', 3);
+INSERT INTO client
+VALUES(406, 'FedEx', 2);
+-- WORKS_WITH
+INSERT INTO works_with
+VALUES(105, 400, 55000);
+INSERT INTO works_with
+VALUES(102, 401, 267000);
+INSERT INTO works_with
+VALUES(108, 402, 22500);
+INSERT INTO works_with
+VALUES(107, 403, 5000);
+INSERT INTO works_with
+VALUES(108, 403, 12000);
+INSERT INTO works_with
+VALUES(105, 404, 33000);
+INSERT INTO works_with
+VALUES(107, 405, 26000);
+INSERT INTO works_with
+VALUES(102, 406, 15000);
+INSERT INTO works_with
+VALUES(105, 406, 130000);
+-- MOre basic queries
+SELECT first_name AS forename,
+  last_name AS surname
+FROM employee;
+-- Find out all the different genders
+SELECT DISTINCT sex
+FROM employee;
+-- 
+SELECT DISTINCT branch_id
+FROM employee;
+-- 
+SELECT *
+FROM employee
+WHERE first_name IN ('Jim', 'Michael', 'Johnny', 'David');
+-- SQL function
+---------------
+-- Find the number of employees
+SELECT COUNT(emp_id)
+FROM employee;
+SELECT COUNT(super_id)
+FROM employee;
+-- Find the number of female employees born after 1970
+SELECT COUNT(emp_id)
+FROM employee
+WHERE sex = 'F'
+  AND birth_day > '1970-01-01';
+-- FInd avarage of employee salaries
+SELECT AVG(salary)
+FROM employee;
+-- 
+SELECT AVG(salary)
+FROM employee
+WHERE sex = 'M';
+--
+-- total salaries
+SELECT SUM(salary)
+FROM employee;
+-- find how many males and females there are
+SELECT COUNT(sex),
+  sex
+FROM employee
+GROUP BY sex;
+-- Find the total sales of each salesman
+SELECT emp_id,
+  SUM(total_sales) AS total_sales
+FROM works_with
+GROUP BY emp_id;
+-- Find the total amount of money spent by each client
+SELECT client_id,
+  SUM(total_sales) AS total_sales
+FROM works_with
+GROUP BY client_id;
