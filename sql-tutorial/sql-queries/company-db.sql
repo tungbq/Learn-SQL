302b56c2670450a9f88bdac5a8262cf33ef1e371
Tung Bui <tungbq.info@gmail.com>
2022-01-06 21:33:44 +0700

    Add wildcards

--- sql-tutorial/sql-queries/company-db.sql
+++ sql-tutorial/sql-queries/company-db.sql
@@ -281,3 +281,19 @@ SELECT client_id,
   SUM(total_sales) AS total_sales
 FROM works_with
 GROUP BY client_id;
+----Wildcards
+-- % = any # characters, _ = one character
+-- find clients who are in LLC
+SELECT *
+FROM client
+WHERE client_name LIKE '%LLC';
+-- Find any branch suppliers who are in the label business
+SELECT *
+FROM branch_supplier
+WHERE supplier_name LIKE '% Label%';
+-- Find any employee born on the 10th day of the month
+SELECT *
+FROM employee
+WHERE birth_day LIKE '%____-10-__%';
+-- Find any clients who are schools
+SELECT * FROM client WHERE client_name LIKE '%Highschool';
