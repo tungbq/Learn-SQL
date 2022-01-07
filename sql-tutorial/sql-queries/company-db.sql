2844079e56fab09c89ea79971b79f24428e1ae66
Tung Bui <tungbq.info@gmail.com>
2022-01-07 09:16:02 +0700

    Add 'trigger'

--- sql-tutorial/sql-queries/company-db.sql
+++ sql-tutorial/sql-queries/company-db.sql
@@ -384,3 +384,83 @@ DELETE FROM branch
 WHERE branch.branch_id = 2;
 SELECT *
 FROM branch_supplier;
+---------trigger
+CREATE TABLE trigger_test (message VARCHAR(100));
+-- 
+DELIMITER $$ CREATE TRIGGER my_trigger BEFORE
+INSERT ON employee FOR EACH ROW BEGIN
+INSERT INTO trigger_test
+VALUES('TLeo added new employee');
+END $$ DELIMITER;
+--
+INSERT INTO employee
+VALUES(
+    109,
+    'Oscar',
+    'Martinez',
+    '1968-02-19',
+    'M',
+    69000,
+    106,
+    3
+  );
+--
+SELECT *
+FROM trigger_test;
+--
+DELIMITER $$;
+CREATE TRIGGER my_trigger1 BEFORE
+INSERT ON employee FOR EACH ROW BEGIN
+INSERT INTO trigger_test
+VALUES(NEW.first_name);
+END $$ DELIMITER;
+INSERT INTO employee
+VALUES(
+    110,
+    'Kevin',
+    'Malone',
+    '1978-02-19',
+    'M',
+    69000,
+    106,
+    3
+  );
+--- using trigger with condition (if/else)
+DELIMITER $$;
+CREATE TRIGGER my_trigger2 BEFORE
+INSERT ON employee FOR EACH ROW BEGIN IF NEW.sex = 'M' THEN
+INSERT INTO trigger_test
+VALUES('Added new male employee');
+ELSEIF NEW.sex = 'F' THEN
+INSERT INTO trigger_test
+VALUES('Added new female employee');
+ELSE
+INSERT INTO trigger_test
+VALUES('Added other employee');
+END IF;
+END $$;
+-- 
+INSERT INTO employee
+VALUES(
+    112,
+    'Pam',
+    'Beesly',
+    '1988-02-19',
+    'F',
+    69000,
+    106,
+    3
+  );
+-- 
+INSERT INTO employee
+VALUES(
+    114,
+    'Pam2',
+    'Beesly',
+    '1988-02-19',
+    NULL,
+    69000,
+    106,
+    3
+  );
+--
